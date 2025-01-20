1a2f5ccdd0b3cf6746a137b23384efc50cb10b34
BugHunter-X <askzenai@gmail.com>
2025-01-20 07:28:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,22 @@
+```javascript
+const MongoClient = require('mongodb').MongoClient;
+
+async function updateDocument() {
+  const uri = "mongodb://localhost:27017";
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+    const database = client.db('myDatabase');
+    const collection = database.collection('myCollection');
+
+    // Incorrect update operation
+    const result = await collection.updateOne({ name: 'John Doe' }, { $set: { age: 30 }, $inc: { age: 1 } });
+    console.log(result);
+  } finally {
+    await client.close();
+  }
+}
+
+updateDocument();
+```
